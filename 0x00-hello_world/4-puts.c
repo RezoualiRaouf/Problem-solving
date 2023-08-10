035d94eed4e553e0ae64f4d1dc5a5cd14f9ab81a
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-10 16:21:16 -0700

    dodne

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<string.h>
- int main(){ 
- puts("\"Programming is like building a multilingual puzzle");
-return 0;
- } 
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
