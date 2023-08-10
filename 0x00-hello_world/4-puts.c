3f640db9b5f284ace6c069ef7fea275a419a5c4d
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-10 16:23:13 -0700

    dodne

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
