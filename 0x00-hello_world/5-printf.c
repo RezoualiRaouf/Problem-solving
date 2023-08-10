696f3576a0d93f6c03cb45d9493596496e7e6035
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-10 16:30:09 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a message using the print function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
