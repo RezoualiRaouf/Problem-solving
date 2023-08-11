117f7344ade10e3e58cc98e0240e1e85377ca9b5
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-11 11:44:28 -0700

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet starting with a to b
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+	putchar(letter);
+	}
+	for (letter = 'A' ; letter <= 'Z' ; letter++)
+        {
+        putchar(letter);
+        }
+	printf("\n");
+	return (0);
+}
