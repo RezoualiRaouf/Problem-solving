f4f629ae3513a5af79466949efbb77c8fb22351f
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-13 07:45:57 -0700

    ff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints numbers from 01 to 98
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = i + 1 ; j <= 9 ; j++)
+		{
+		putchar('0' + i);
+		putchar('0' + j);
+			if (i < 8 || j < 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
