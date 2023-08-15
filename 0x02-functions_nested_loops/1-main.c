0712dc548290cb495f5068f9f9539293dfd1a794
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-15 15:11:41 -0700

    l

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
