dbe6013d3b6912cbccb6c9fdd10212858d179227
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-16 07:28:37 -0700

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+void jack_bauer(void)
+{
+	int a ,b ,c , d;
+for (a = 0 ; a <= 2 ; a++)
+	{
+	for (b = 0 ; b <= 3 ; b++)
+	{
+		for (c = 0 ; c <= 5 ; c++)
+		{
+			for (d = 0 ; d <= 9 ; d++)
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+				_putchar(':');
+				_putchar(c + '0');
+				_putchar(d + '0');
+				_putchar('\n');
+	}
+	}
+		}
+			}
+}
