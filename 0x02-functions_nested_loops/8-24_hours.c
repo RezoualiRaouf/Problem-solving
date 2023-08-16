d7977c34a3ea634a2123ff5f6ff228d054cca989
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-16 08:31:22 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,39 +8,18 @@ void jack_bauer(void)
 
 	for (a = 0 ; a <= 2 ; a++)
 	{
-		if (a < 2)
+		for (b = 0 ; b <= (a < 2 ? 9 : 3) ; b++)
 		{
-			for (b = 0 ; b <= 9 ; b++)
+			for (c = 0 ; c <= 5 ; c++)
 			{
-				for (c = 0 ; c <= 5 ; c++)
+				for (d = 0 ; d <= 9 ; d++)
 				{
-					for (d = 0 ; d <= 9 ; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
-		}
-		else if (a == 2)
-		{
-			for (b = 0 ; b <= 3 ; b++)
-			{
-				for (c = 0 ; c <= 5 ; c++)
-				{
-					for (d = 0 ; d <= 9 ; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 		}
