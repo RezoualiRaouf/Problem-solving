1786e604ed1ab37c012893886ce886d0d81b7a27
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-16 06:55:08 -0700

    dono

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer value
+ *
+ * Return: The absolute value of n
+ */
+int print_last_digit(int n)
+{
+	int digit;
+
+	digit = n % 10;
+return (digit);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int n);
 #endif
