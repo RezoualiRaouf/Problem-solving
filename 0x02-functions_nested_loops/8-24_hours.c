c939165841dd3c6699d15ee78f32d88bea4489e2
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-16 08:16:59 -0700

    d

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,48 @@
 #include "main.h"
-
-
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
 void jack_bauer(void)
 {
-	int a ,b ,c , d;
-for (a = 0 ; a <= 2 ; a++)
+	int a, b, c, d;
+
+	for (a = 0 ; a <= 2 ; a++)
 	{
-	for (b = 0 ; b <= 3 ; b++)
-	{
-		for (c = 0 ; c <= 5 ; c++)
+		if (a < 2)
 		{
-			for (d = 0 ; d <= 9 ; d++)
+			for (b = 0 ; b <= 9 ; b++)
 			{
-				_putchar(a + '0');
-				_putchar(b + '0');
-				_putchar(':');
-				_putchar(c + '0');
-				_putchar(d + '0');
-				_putchar('\n');
-	}
-	}
-		}
+				for (c = 0 ; c <= 5 ; c++)
+				{
+					for (d = 0 ; d <= 9 ; d++)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
 			}
+		}
+		else if (a == 2)
+		{
+			for (b = 0 ; b <= 3 ; b++)
+			{
+				for (c = 0 ; c <= 5 ; c++)
+				{
+					for (d = 0 ; d <= 9 ; d++)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
