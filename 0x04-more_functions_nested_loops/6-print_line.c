17790570895aaba452bdbad91fab0bc5d6998655
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-18 15:16:58 -0700

    done with task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - Print a line of underscores
+ * @n: Number of underscores to print
+ */
+void print_line(int n)
+{
+int i;
+
+if (n > 0)
+{
+	for (i = 0 ; i <= n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+else
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,8 @@ int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void more_numbers(void);
+void print_line(int n);
 void print_numbers(void);
 void print_most_numbers(void);
+
 #endif
