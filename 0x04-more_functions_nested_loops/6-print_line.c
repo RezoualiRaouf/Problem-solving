ba4163e2ef211ccfcec1a0567e128c3f402ec019
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-18 15:32:28 -0700

    d

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * print_line - Print a line of underscores
- * @n: Number of underscores to print
+ * print_line - Print a straight line in the treminal
+ * @n: Number of times the character _ to be printed
  */
 void print_line(int n)
 {
 int i;
 
-if (n > 0)
-{
-	for (i = 0 ; i <= n ; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
-else if (n <= 0)
-{
-	_putchar('\n');
-}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
