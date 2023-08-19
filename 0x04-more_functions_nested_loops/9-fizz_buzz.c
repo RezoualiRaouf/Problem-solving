56dde6438030e9c9f2c9e89bd087984dc41e2b02
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-19 13:02:00 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function implements the FizzBuzz problem, printing
+ * numbers from 1 to 100 with specific replacements.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100 ; i++)
+{
+	if (i % 3 == 0)
+		printf("Fizz ");
+	else if (i % 5 == 0)
+		printf("Buzz ");
+	else if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz ");
+	else
+		printf("%d ", i);
+}
+printf("\n");
+return (0);
+}
