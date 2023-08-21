2aaab692986f08fcbb0ccb57b7aefd83a14573e1
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-21 14:49:31 -0700

    done with task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string
+ *@s: points to a string
+ */
+void rev_string(char *s)
+{
+int n = 0, i;
+char *p = s, temp;
+
+while (*p != '\0')
+{
+	n++;
+	p++;
+}
+p--;
+for (i = 0 ; i <= n / 2 ; i++)
+{
+	temp = *s;
+	*s = *p;
+	*p = temp;
+	s++;
+	p--;
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-
+void rev_string(char *s);
 
 
 #endif
