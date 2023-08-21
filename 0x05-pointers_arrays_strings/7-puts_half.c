e14b1f865b4e294498eb6bef5706c57890b4d362
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-21 16:16:00 -0700

    done with task 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-
+ * puts_half - Prints the second half of a string
+ * @str: The input string
  */
 void puts_half(char *str)
 {
@@ -17,7 +18,7 @@ if (l % 2 == 0)
 else
 	p = str + (l - 1) / 2;
 while (*p != '\0')
-{	
+{
 	_putchar(*p);
 	p++;
 }
