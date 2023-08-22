d248bf1ffee1b2ed2999a2882af59abfc894cc49
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-22 15:37:32 -0700

    done with task 7 but it's not my work

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,29 @@
 #include "main.h"
+
 /**
- *puts2 - prints every other character of a string, starting
- * with the first character
- *@str: points to a string
+ * puts2 - prints every other character of a string
+ * @str: string to print its characters
  */
 void puts2(char *str)
 {
-while (*str != '\0')
-{
-	_putchar(*str);
-	str = str + 2;
-}
-_putchar('\n');
+	int length;
+	int i;
+
+	if (str == NULL)
+	{
+		return;
+	}
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
