c04311e271b4514fe326fec294d3d99c4cf263c0
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-24 03:17:17 -0700

    done with task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string with specified number of characters.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of characters to copy.
+ * Return: Pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    char *point_dest = dest;
+
+    while (n > 0 && *src != '\0')
+    {
+        *dest = *src;
+        src++;
+        dest++;
+        n--;
+    }
+
+    while (n > 0)
+    {
+        *dest = '\0';
+        dest++;
+        n--;
+    }
+
+    return (point_dest);
+}
+
