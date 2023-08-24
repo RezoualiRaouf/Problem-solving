d5055851b158c03ed2b5195ba8656783345798be
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-08-24 10:59:19 -0700

    done with task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
+/**
+ * _strcmp - compare two strigs.
+ *@s1: points to the first string.
+ *@s2: points to the second string.
+ *Return: integer positive if s1 > s2, negative if s1 < s2 , 0 if s1 = s2.
+ */
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, sum = 0;
 
-while (s1[i] !='\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-	if(s1[i] > s2[i])
+	if (s1[i] > s2[i])
 	{
 		sum = s1[i] - s2[i];
- 		break;
+		break;
 	}
 	else if (s1[i] < s2[i])
 	{
