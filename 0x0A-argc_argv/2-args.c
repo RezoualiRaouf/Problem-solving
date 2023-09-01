4bf9b097720a852a113842fe33ef93e69cfa6cbb
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-01 15:30:29 -0700

    done with task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0 ; i < argc ; i++)
 	{
 		printf("%s", argv[i]);
