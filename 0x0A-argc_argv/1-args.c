a76ad5086ecfa90b817544b3069d3657ceb71432
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-01 15:07:05 -0700

    done with task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+printf("%d", argc - 1);
+printf("\n");
+(void)argv;
+return (0);
+}
