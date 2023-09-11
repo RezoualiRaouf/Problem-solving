a9735824068688e240e7fee37b5282012cf031de
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-11 13:58:49 -0700

    done with task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,10 +6,8 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- *
  * Return: A pointer to the newly created dog_t struct, or NULL if it fails.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, name_len = 0, owner_len = 0;
@@ -23,12 +21,16 @@ while (owner[owner_len] != '\0')
 pd->name = malloc(sizeof(name) + 1);
 pd->owner = malloc(sizeof(pd->owner) + 1);
 if (pd->owner == NULL || pd->name == NULL)
+{
+	free(pd->owner);
+	free(pd->name);
+	free(pd);
 	return (NULL);
+}
 for (i = 0 ; i <= name_len ; i++)
 	pd->name[i] = name[i];
 for (i = 0 ; i <= owner_len ; i++)
 	pd->owner[i] = owner[i];
 pd->age = age;
-
 return (pd);
 }
