bd4d0fc509b8b50ba0aa4717705c34d29656abdb
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-11 07:37:15 -0700

    done with task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This dir is for learning structures in c int the alx program
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner's name of the dog.
+ *
+ *Description: This structure contains basic information about a dog, including
+ * its name, age, and owner's name.
+ */
+struct dog
+{
+char  *name;
+float age;
+char *owner;
+};
+#endif
