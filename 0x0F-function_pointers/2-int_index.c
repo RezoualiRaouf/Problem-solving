948dbb6885f832042c8dd02294953fec404d22ff
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-13 10:36:27 -0700

    done with task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * int_index - Searches for an integer.
@@ -11,14 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, index, find = 0;
 
-if (array == NULL || cmp == NULL)
-	exit(0);
+if (array == NULL)
+	return (0);
 if (size <= 0)
-	exit(-1);
+	return (-1);
 for (i = 0 ; i < size ; i++)
 {
 	index = i;
-	if (cmp(array[i]) == 1)
+	if (cmp(array[i]) != 0)
 	{
 		find = 1;
 		break;
