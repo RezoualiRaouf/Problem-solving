f2adfb90e1d9d257bacd80425281893170addf9d
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-14 07:01:47 -0700

    header file : ceated

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+
+
+
+
+
+
+
+
+
+#endif
