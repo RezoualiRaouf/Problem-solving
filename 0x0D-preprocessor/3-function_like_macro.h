3bed5c44f74b8c15818ccc9b6b1f7008ea2df7c5
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-15 12:15:10 -0700

    task 3 : done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,15 @@
 #ifndef PI_H
 #define PI_H
+/*
+ * ABS macro: Calculate the absolute value of a number.
+ *
+ * This macro takes a numeric input value 'x' and returns its absolute value.
+ *
+ * Parameters:
+ *    x (int): The input numeric value.
+ *
+ * Returns:
+ *    int: The absolute value of 'x'.
+ */
 #define ABS(x)((x) >= 0 ? (x) : (-x))
 #endif
