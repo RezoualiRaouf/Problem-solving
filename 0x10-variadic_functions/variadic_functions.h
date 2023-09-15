49e09783034e40eda7ac9b46fde9eebf5ec93a55
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-15 08:08:25 -0700

    header file : done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,13 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct data - Data structure to map format specifiers to functions
+ * @ch: The format specifier
+ * @f: A pointer to the corresponding function
+ */
+typedef struct data
+{
+const char *ch;
+void (*f)(va_list ptlist);
+} data;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-
-
-
-
 #endif
