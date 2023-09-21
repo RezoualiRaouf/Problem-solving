900c265ce6cf68e312a150c391b5ee497b80da58
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-21 07:04:45 -0700

    function 2 : done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new = *head;
 unsigned int i = 0;
-
+	
+	if (str == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
