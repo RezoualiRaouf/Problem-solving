909a1e65f2dcfb466aecc6523b7dcd4f7b6effe5
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-09-21 12:07:13 -0700

    function 100 : done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * first_to_exct - Function that runs before main.
+ */
+void first_to_exct(void) __attribute__((constructor));
+/**
+ * first_to_exct - Function that runs before main.
+ */
+void first_to_exct(void)
+{
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore myhouse upon my back!\n");
+}
