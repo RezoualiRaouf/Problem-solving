56bcd84e6507a5128729c7cc72be7e7443e7581d
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-10-02 13:31:08 -0700

    function 1 : done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _sizeof - Computes the length of a string.
+ * @text: A pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _sizeof(char *text)
+{
+int i = 0;
+
+while (text[i] != '\0')
+	i++;
+text[i] = '\0';
+return (i);
+}
+/**
+ * create_file - Creates a file and writes text content to it.
+ * @filename: The name of the file to create or truncate.
+ * @text_content: The content to write to the file.
+ *
+ * Return: On success, return 1. On failure, return -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int pt, bwrite;
+
+	if (filename == NULL)
+		return (-1);
+	pt = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (pt == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(pt);
+		return (-1);
+	}
+	bwrite = write(pt, text_content, _sizeof(text_content));
+	if (bwrite == -1)
+	{
+		close(pt);
+		return (-1);
+	}
+	close(pt);
+return (1);
+}
