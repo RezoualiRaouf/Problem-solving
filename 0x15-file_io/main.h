f6f10db4e65d3e21a4aef98d3a4bdb7361e25d36
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-10-02 13:30:43 -0700

    header file : done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 
 int _putchar(char c);
+int _sizeof(char *text);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
