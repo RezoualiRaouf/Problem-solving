c34cf19b7a8d9b1bbb71aa3e9279b78568193634
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-10-03 13:56:05 -0700

    header file : done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#define BUFFERSIZE 1024
 int _putchar(char c);
 int _sizeof(char *text);
+void exit_with_error_int(int code, const char *message, ssize_t arg);
+void exit_with_error_char(int code, const char *message, const char *arg);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
