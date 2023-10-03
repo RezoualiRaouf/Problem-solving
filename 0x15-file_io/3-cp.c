aac80855dd2e0e80b4bf3669fa39dba51d35eb68
RezoualiRaouf <raoufrezouali7@gmail.com>
2023-10-03 13:56:35 -0700

    program 3 : done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,25 @@
 #include "main.h"
 /**
- * _sizeof - Computes the length of a string.
- * @text: A pointer to the string.
- *
- * Return: The length of the string.
+ * exit_with_error_char - Print an error message and exit with a specified code
+ * @code: The exit code to use.
+ * @message: The error message format string.
+ * @arg: The argument to insert into the error message.
  */
-int _sizeof(char *text)
+void exit_with_error_char(int code, const char *message, const char *arg)
 {
-int i = 0;
-
-while (text[i] != '\0')
-	i++;
-return (i);
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
+/**
+ * exit_with_error_int - Print an error message and exit with a specified code.
+ * @code: The exit code to use.
+ * @message: The error message format string.
+ * @arg: The argument to insert into the error message.
+ */
+void exit_with_error_int(int code, const char *message, ssize_t arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
 }
 /**
  * main - Entry point of the program.
@@ -22,47 +30,38 @@ return (i);
  */
 int main(int ac, char **av)
 {
-int pt, pt2, bread, bwrite;
-char buffer[1024];
+int pt, pt2;
+ssize_t by_read, by_write;
+char buffer[BUFFERSIZE];
 
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	pt = open(av[1], O_RDONLY);
-	if (pt == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-	exit(98);
-	}
-	bread = read(pt, buffer, 1024);
-	if (bread == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-	exit(98);
-	}
-	pt2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (pt2 == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-	exit(99);
-	}
-	bwrite = write(pt2, buffer, _sizeof(buffer));
-	if (bwrite == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-	exit(99);
-	}
-	if (close(pt) == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", pt);
-	exit(100);
-	}
-	if (close(pt2) == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", pt2);
-	exit(100);
-	}
+if (ac != 3)
+{
+	exit_with_error_char(97, "Usage: cp file_from file_to\n", "");
+}
+pt = open(av[1], O_RDONLY);
+if (pt == -1)
+{
+	exit_with_error_char(98, "Error: Can't read from file %s\n", av[1]);
+}
+pt2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (pt2 == -1)
+{
+	exit_with_error_char(99, "Error: Can't write to %s\n", av[2]);
+}
+while ((by_read = read(pt, buffer, BUFFERSIZE)) > 0)
+{
+	by_write = write(pt2, buffer, by_read);
+	if (by_write == -1)
+		exit_with_error_char(99, "Error: Can't write to %s\n", av[2]);
+}
+if (by_read == -1)
+	exit_with_error_char(98, "Error: Can't read from file %s\n", av[1]);
+
+if (close(pt) == -1)
+	exit_with_error_int(100, "Error: Can't close fd %d\n", by_read);
+
+if (close(pt2) == -1)
+	exit_with_error_int(100, "Error: Can't close fd %d\n", by_write);
+
 return (0);
 }
